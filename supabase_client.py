f635b2f78cb14b2a141dd884d92fdec35dce8931
saniya <saniyabanubvr@gmail.com>
2025-05-17 15:53:40 +0530

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -12,8 +12,8 @@ def get_client(access_token=None, refresh_token=None):
         Configured Supabase client
     """
     # Get credentials from environment variables
-    supabase_url = 'https://bmoicuppczldcjqotnuw.supabase.co'
-    supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtb2ljdXBwY3psZGNqcW90bnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzNzI5ODcsImV4cCI6MjA1Nzk0ODk4N30.dqJsgStt0HeTE5mA6IJ__bTJbei9D2OnOo0B2rgE0T4"
+    supabase_url = 
+    supabase_key = 
     
     # Create the base client
     client = create_client(supabase_url, supabase_key)
